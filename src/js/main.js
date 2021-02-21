740692e9d0481269f93ed2c37b019ca5df0ec7c1
Hendrik Sachse <hendrik.sachse@gmail.com>
2021-02-21 20:49:11 +0100

    reset

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 import './base/ns';
-import './base/factory';
 import './utils/ajax';
 import './utils/number';
-import './controller/form';
 
 import './../css/styles.css';
 
@@ -12,31 +10,117 @@ import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
 import VueOnsen from 'vue-onsenui';
 
-Vue.use(VueOnsen);
-
 (function(app){
+    var data = null,
+        language = 'ja';
 
-    function loadView(){
+    Vue.use(VueOnsen);
 
-    }
-
-    loadView('form');
-
-
-    // new Vue({
-    //     el: '#vocabulary',
+    new Vue({
+        el: '#vocabulary',
     
-    //     /**
-    //      * vue is ready
-    //      */
-    //     created: function(){
-    //         var _me = this,
-    //             _promise = app.utils.ajax.get('api/data');
+        data: {
+            correctAmount: 0,
+            errorAmount: 0,
+    
+            question: '',
+            answer: '',
+            answerColl: [],
 
-    //         _promise.done(function(pData){
-    //             data = pData[language];
-    //             _me.initQuestion();
-    //         });
-    //     },
-    // });
+            showValidation: false,
+            isValid: false,
+        },
+
+        /**
+         * vue is ready
+         */
+        created: function(){
+            var _me = this,
+                _promise = app.utils.ajax.get('api/data');
+
+            _promise.done(function(pData){
+                data = pData[language];
+                _me.initQuestion();
+            });
+        },
+
+        methods: {
+            /**
+             * show a new question
+             */
+            initQuestion: function(){
+                var _q = this.generateQuestion();
+                this.showValidation = false;
+                
+                this.question = _q[0];
+                this.answer = '';
+                this.answerColl = _q.slice().splice(1, _q.length -1);
+                this.$nextTick(function(){
+                    this.$refs.answer.focus();
+                });
+            },
+
+            /**
+             * get a random q/a-pair from data
+             */
+            generateQuestion: function(){
+                var index = app.utils.number.randomIntFromInterval(0, data.length -1);
+                
+                return data[index].split(';');
+            },
+
+            /**
+             * validate input
+             */
+            validate: function(){
+                var _answerVal = this.answer,
+                    _answerColl = this.answerColl,
+                    _isValid = false;
+                
+                if(_answerColl.indexOf(_answerVal) !== -1){
+                    _isValid = true;
+                }
+        
+                this.isValid = _isValid;
+                this.showValidation = true;
+        
+                return _isValid;
+            },
+
+            showData: function(){
+
+            },
+
+            /**
+             * submit answer
+             */
+            onQuestFormSubmit: function(){
+                
+                var _valid = this.validate();
+                if(_valid){
+                    this.correctAmount = this.correctAmount + 1;
+                    this.$nextTick(function(){
+                        this.$refs.next.focus();
+                    });
+                }else{
+                    this.errorAmount = this.errorAmount + 1;
+                    this.answer = '';
+                }
+            },
+
+            /**
+             * meh, another!
+             */
+            onSkipFormSubmit: function(){
+                this.initQuestion();
+            },
+
+            /**
+             * solved, another!
+             */
+            onResultFormSubmit: function(){
+                this.initQuestion();
+            }
+        }
+    });
 })(app);
